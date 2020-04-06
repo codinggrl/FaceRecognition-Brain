66e7d04975f91bc9bf56982ed0fdb633f25642d9
Ivana Piljic <piljic.ivana@gmail.com>
2020-04-06 20:17:32 +0100

    Front-end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area : 800
+      }
+    }
+  }
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event);
+  }
+
   render() {
     return (
       <div className="App">
+       <Particles className='particles'
+            params={particlesOptions}
+        />
         <Navigation />
         <Logo />
-         {/*<ImageLinkForm />
-        <FaceRecognition />*/}
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
